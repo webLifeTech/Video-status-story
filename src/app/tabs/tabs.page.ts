a829a08e0972a1a846bcdd2af87dd7fee2cb66ea
Paras Gogdani <fivverprojectgit@gmail.com>
2021-04-17 21:13:44 +0530

    '-paras'

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(
+    public router : Router
+  ) {}
+
+  tab2(){
+    this.router.navigate(['/tabs/tab2'])
+  }
 
 }
